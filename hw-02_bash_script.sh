0864d83e9d73e057d7e9abaa8018bd48cf3bad1b
andreamia <spirirted.away@gmail.com>
2025-01-21 04:30:06 +0000

    update of bash script

--- hw-02_bash_script.sh
+++ hw-02_bash_script.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 green='\e[32m'
-red='\e[31m'
 clear='\e[0m'
 users_file="/etc/passwd"
-users_number=`wc -l $users_file | awk '{print $1;}'`
-prompt="${green}List of all ${red}$users_number ${green}users from $users_file :${clear}"
+users_cli="/bin/bash"
+prompt="${green}List of all users from $users_file file with $users_cli shell:${clear}"
 echo -e $prompt
-cat -n $users_file | awk -F : '{print $1}'
+cat $users_file | grep $users_cli
